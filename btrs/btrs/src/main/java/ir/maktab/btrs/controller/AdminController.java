fee631a69f0d6df8f521dae9dbba827df2e5cacf
Reza-Rahmani1378 <reza6273757@gmail.com>
2021-10-27 21:12:15 +0330

    add Admin

---
+++ btrs/btrs/src/main/java/ir/maktab/btrs/controller/AdminController.java
@@ -0,0 +1,13 @@
+//package ir.maktab.btrs.controller;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//
+//@Controller
+//public class AdminController {
+//
+//    @GetMapping("/tickets")
+//    public String tickets(){
+//        return "tickets";
+//    }
+//}
---
+++ btrs/btrs/src/main/java/ir/maktab/btrs/models/Admin.java
@@ -0,0 +1,15 @@
+package ir.maktab.btrs.models;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Admin extends User {
+
+}
