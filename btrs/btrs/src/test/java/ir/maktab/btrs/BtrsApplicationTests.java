3b399ab8857f1c5029c0cb1dcd3d4e4b813f317a
Reza-Rahmani1378 <reza6273757@gmail.com>
2021-10-27 21:16:06 +0330

    add base of program

---
+++ btrs/btrs/src/test/java/ir/maktab/btrs/BtrsApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.maktab.btrs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BtrsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
